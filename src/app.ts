906f93212ada48ecfdecb45ad9c6ef7df360a8d8
unknown <tauft@aol.com>
2024-05-21 01:40:37 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World from other side!");
 });
 
 export default app;
